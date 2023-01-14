472c0ebfb50857b6589830674647881b089aee10
tylersfoot <tylerdepa8@gmail.com>
2023-01-14 18:08:01 -0500

    correct SDL keycodes

--- Charts/typeracer/mod.lua
+++ Charts/typeracer/mod.lua
@@ -15,9 +15,9 @@ function create()
     converted = {} -- array to hold characters
 
     -- converts str to SDL keycode using reference array and puts each character in the `t` array
-    reference = {a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10,
-        k = 11, l = 12, m = 13, n = 14, o = 15, p = 16, q = 17, r = 18, s = 19, t = 20, u = 21,
-        v = 22, w = 23, x = 24, y = 25, z = 26, [" "]= 27}
+    reference = {a = 97, b = 98, c = 99, d = 100, e = 101, f = 102, g = 103, h = 104, i = 105, j = 106,
+        k = 107, l = 108, m = 109, n = 110, o = 111, p = 112, q = 113, r = 114, s = 115, t = 116, u = 117,
+        v = 118, w = 119, x = 120, y = 121, z = 122, [" "]= 32}
 
     -- array with the characters, unconverted; used for making sprites
     characters = {}
